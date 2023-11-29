c507818a7e923222a5c6e7bda85382737a57f6e5
fdicera <67300276+fdicera@users.noreply.github.com>
2023-11-29 04:21:04 +0000

    Finished all associations.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -22,15 +22,16 @@ class Actor < ApplicationRecord
     #return the_many
   #end
 
-  def filmography
-    the_many = Array.new
+  has_many(:filmography, through: :characters, source: :movie)
+ #def filmography
+  #  the_many = Array.new
 
-    self.characters.each do |joining_record|
-      destination_record = joining_record.movie
+   # self.characters.each do |joining_record|
+    #  destination_record = joining_record.movie
 
-      the_many.push(destination_record)
-    end
+     # the_many.push(destination_record)
+    #end
 
-    return the_many
-  end
+    #return the_many
+  #end
 end
